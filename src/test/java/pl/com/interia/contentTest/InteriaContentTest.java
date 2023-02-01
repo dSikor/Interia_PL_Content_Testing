76c0cebf395809a9b81579dc2f720a74afaa8b45
dSikor <dsikora08@gmail.com>
2023-02-02 00:54:38 +0100

    added two arraylist for news title from web and exel file

--- src/test/java/pl/com/interia/contentTest/InteriaContentTest.java
+++ src/test/java/pl/com/interia/contentTest/InteriaContentTest.java
@@ -25,40 +25,30 @@ import java.util.Map;
 public class InteriaContentTest {
     WebDriver driver;
     List<String> newsTitles;
+    List<String> newsTitlesGetFromWeb;
+
     Workbook workbook;
     Sheet sheet;
     @BeforeTest
     public void beforeTest() throws IOException {
+
+        newsTitles = new ArrayList<String>();
+        newsTitlesGetFromWeb=new ArrayList<String>();
+
         try (FileInputStream file = new FileInputStream(new File("C:\\Users\\Damiano\\IdeaProjects\\InteriaPLContentTest\\src\\test\\java\\pl\\com\\interia\\contentTest\\News.xlsx")))
         {
             workbook = new XSSFWorkbook(file);
         }
         sheet = workbook.getSheetAt(0);
-        Map<Integer, List<String>> data = new HashMap<>();
-        int i = 0;
         for (Row row : sheet) {
-            data.put(i, new ArrayList<String>());
-            for (Cell cell : row) {
-                data.get(i).add(cell.getRichStringCellValue().getString());
-                System.out.println(cell.getRichStringCellValue().getString().isEmpty());
-            }
-            i++;
+            newsTitles.add(row.getCell(0).toString());
         }
+        newsTitles.remove(0);
 
         System.setProperty("webdriver.chrome.driver", "C:/Selenium drivers/chromedriver.exe");
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.interia.pl/");
-        newsTitles = new ArrayList<String>();
-        newsTitles.add("Ukraina: Rosja po raz pierwszy ostrzelała Kijów rakietami S-400");
-        newsTitles.add("Kissinger w Davos: Koncepcja neutralnej Ukrainy nie ma dłużej sensu ");
-        newsTitles.add("Pierwsze takie spotkanie dowódców z USA i Ukrainy. Odbyło się w Polsce");
-        newsTitles.add("Greta Thunberg zatrzymana przez niemiecką policję");
-        newsTitles.add("Sensacyjny początek debiutu Bereszyńskiego w Napoli!");
-        newsTitles.add("Media ujawniają: Gierasimow dopuścił się zbrodni wojennej w Czeczenii");
-        newsTitles.add("Miedwiediew komentuje szczyt w Davos. \"Ten polski Duda\"");
-        newsTitles.add("KO: Fundacja powiązana z politykami PiS dostała 5 mln zł i kupiła willę");
-        newsTitles.add("Afganistan: Publiczna chłosta na stadionie. \"Setki gapiów na trybunach\"");
     }
     @AfterTest
     public void afterTest(){
@@ -81,19 +71,13 @@ public class InteriaContentTest {
         //******************************************************************//
         //Checking if the news is up to date
         List<WebElement> listOfTitleNews = driver.findElements(By.cssSelector("li.wiadspec-li > a > span.tile-span > span"));
-        System.out.println("Number of elements:" +listOfTitleNews.size());
-        System.out.println("First Title:" +newsTitles.get(8));
-        System.out.println("First Title:" +listOfTitleNews.get(8).getText());
-
-        if (listOfTitleNews.get(8).getText().matches(newsTitles.get(8))) {System.out.println("TEST 3 - Tytuł jest poprawny");
-        }else {System.out.println("TEST 3 - Tytuł do dupy");}
-
-        List<String> newsTitlesGetFromWeb=new ArrayList<String>();
         listOfTitleNews.forEach(e->{
             newsTitlesGetFromWeb.add(e.getText());
         });
-
-        if (newsTitlesGetFromWeb.equals(newsTitles)){System.out.println("TEST 4 - Tytuł OK");}
+        if (newsTitlesGetFromWeb.containsAll(newsTitles)){
+            System.out.println("TEST 2 - Newsy zgodnie z plikem exel");
+            System.out.println("TEST 2 - Tytuł OK");
+        }
         //*******************************************************************//
     }
     @Test(priority = 3)
