0ca0a138315f784eeba245538305cac79420923d
dSikor <dsikora08@gmail.com>
2023-02-14 23:36:36 +0100

    created new object of CreateNewAccountPage class, call new function to set parameters

--- src/test/java/pl/com/interia/contentTest/InteriaRegistrationTest.java
+++ src/test/java/pl/com/interia/contentTest/InteriaRegistrationTest.java
@@ -6,9 +6,16 @@ import pl.com.interia.webObject.*;
 public class InteriaRegistrationTest extends RegistrationUser {
 
     @Test
-
     public void registrationUserTest(){
         driver.get(baseURL);
+
+        CreateNewAccountPage cnap = new CreateNewAccountPage(driver);
+
+        //Registration new user
+        cnap.setUserName(userName);
+        cnap.setUserSurname(userSurname);
+        cnap.setUserPassword(password);
+
     }
 
 
