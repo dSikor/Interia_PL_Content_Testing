5b61a7385ead2f5fa7a6b9fc731fc2f6af9d994a
dSikor <dsikora08@gmail.com>
2023-03-09 23:42:46 +0100

    added operation opening of login web page

--- src/test/java/pl/com/interia/contentTest/InteriaContentTest.java
+++ src/test/java/pl/com/interia/contentTest/InteriaContentTest.java
@@ -85,7 +85,8 @@ public class InteriaContentTest {
     public void canNotLoginWithRandomCredential(){
         // Checking log in and log out
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/header/nav/ul/li[1]/a")).click();
+        driver.get(loginUserPage);
+//        driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/header/nav/ul/li[1]/a")).click();
         driver.manage().window().maximize();
         driver.findElement(By.id("email")).sendKeys("sdasdas@interia.pl");
         driver.findElement(By.id("password")).sendKeys("dsasadasd");
