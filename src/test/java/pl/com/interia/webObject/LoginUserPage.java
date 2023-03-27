232870f9fa6c62e822313f9f4ac5d7cca713cfa1
dSikor <dsikora08@gmail.com>
2023-03-27 23:14:01 +0200

    created LoginUserPage class

---
+++ src/test/java/pl/com/interia/webObject/LoginUserPage.java
@@ -0,0 +1,43 @@
+package pl.com.interia.webObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginUserPage {
+    WebDriver cnap_driver;
+
+    public LoginUserPage(WebDriver mdriver)
+    {
+        this.cnap_driver=mdriver;
+        PageFactory.initElements(mdriver,this);
+    }
+
+    @FindBy(id="email")
+    WebElement userEmail;
+
+    @FindBy(id="password")
+    WebElement userPassword;
+
+
+    @FindBy(xpath = "//button[@class=\"btn\"]")
+    WebElement logInButton;
+
+    public void setUserEmail(String email) {
+        userEmail.sendKeys(email);
+    }
+    public WebElement getUserEmail() {
+        return userEmail;
+    }
+    public void setUserPassword(String password) {
+        userPassword.sendKeys(password);
+    }
+
+    public void logIn()
+    {
+        Actions actions = new Actions(cnap_driver);
+        actions.moveToElement(logInButton).click().perform();
+    }
+}
